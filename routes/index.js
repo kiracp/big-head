769f7063f7862c8098d11d846b64df7f05e6d1e8
kcp288 <kira.prentice@nyu.edu>
2016-01-30 18:44:14 -0500

    ratio

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/data', function(req,res,next){
 		// Message is whatever is printed by the python function
 	pyshell.on('message', function (message) {		  
 		console.log(message);
-		var msg = message.slice(1, message.length-1);
+		var msg = message.slice(1, message.length-4);
 		var msg = message.split(',');
 
 		console.log(msg);
@@ -33,9 +33,47 @@ router.post('/data', function(req,res,next){
 		for (var i = 0; i< msg.length; i++) {
 			var value = msg[i].split(":");
 			console.log("value:", value);
-			newArray[i] = value[0].slice(2, value[0].length-1) + ':' + value[1]
+			var lilArray = [];
+			lilArray[0] = value[0].slice(2, value[0].length-1);
+
+			lilArray[1] = +value[1];
+
+			if (isNaN(lilArray[1])) {
+				lilArray[1] = 0;
+			}
+			newArray[i] = lilArray;
 		}
 		console.log(newArray);
+
+		var total = 0;
+		var total1s = 0;
+		var total1p = 0;
+		var total2s = 0;
+		var total2p = 0;
+		var total3s = 0;
+		var total3p = 0;
+
+		for (var i = 0; i< newArray.length; i++) { 
+			total = total + newArray[i][1];
+		}
+
+		for (var i = 0; i< newArray.length; i++) { 
+			if (newArray[i][0] == "mine" || 
+				newArray[i][0] == "i" ||
+				newArray[i][0] == "me" ||
+				newArray[i][0] == "my" ||
+				newArray[i][0] == "myself"){
+					total1s = total1s + newArray[i][1];
+			}
+		}
+
+		console.log(total);
+		console.log(total1s);
+
+		var ratio = (total1s + 0.0)/total;
+		console.log(ratio )
+
+
 
 		var big = false;
 
